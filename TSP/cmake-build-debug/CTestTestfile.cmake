09e8d4aa7d9b168b7dbdd6655ae41f657d7bd38a
Billy <billyjrsantos@outloook.com>
2025-09-28 17:45:38 -0300

    Added CMake

---
+++ TSP/cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/repos/CLionProjects/ImplementacaoAlgoritmosGeneticos/TSP
+# Build directory: C:/repos/CLionProjects/ImplementacaoAlgoritmosGeneticos/TSP/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("C:/repos/CLionProjects/ImplementacaoAlgoritmosGeneticos/TSP/cmake-build-debug/tests-b12d07c_include.cmake")
+subdirs("_deps/catch2-build")
